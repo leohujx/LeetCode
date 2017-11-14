12182d76a2c8cbfc3574285d28bd8b8faa2cff99
leohujx <leohujx@163.com>
2017-11-14 21:29:49 +0800

    109 is added

---
+++ 412.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+
+# 412. Fizz Buzz
+
+# https://leetcode.com/problems/fizz-buzz/description/
+
+'''
+大水题
+'''
+
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                res.append("FizzBuzz")
+            elif i % 3 == 0:
+                res.append("Fizz")
+            elif i % 5 == 0:
+                res.append("Buzz")
+            else:
+                res.append(str(i))
+
+        return res
+
+
+
